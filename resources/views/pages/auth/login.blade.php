b7189526822aa07a43944ba212682f4e5b64a9ae
rohmannu2904 <rohman.nurafan13@gmail.com>
2021-01-07 11:00:39 +0700

    Form Login Revisi

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -34,17 +34,11 @@
               <form class="forms-sample" method="POST" action="{{ route('loginPost') }}">
                 <div class="form-group">
                   <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" class="form-control" name="username" id="exampleInputEmail1" placeholder="Email">
+                  <input type="email" class="form-control" autofocus autocomplete="off" name="username" id="exampleInputEmail1" placeholder="Email">
                 </div>
                 <div class="form-group">
                   <label for="exampleInputPassword1">Password</label>
                   <input type="password" class="form-control" name="password" id="exampleInputPassword1" autocomplete="current-password" placeholder="Password">
-                </div>
-                <div class="form-check form-check-flat form-check-primary">
-                  <label class="form-check-label">
-                    <input type="checkbox" class="form-check-input">
-                    Remember me
-                  </label>
                 </div>
                 <div class="mt-3">
                   <button type="submit" class="btn btn-primary mr-2 mb-2 mb-md-0">
